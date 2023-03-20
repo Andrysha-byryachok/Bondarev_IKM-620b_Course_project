ca1c74ae8a8059b7a58f07202d240c4be894107a
Andrysha-byryachok <bondarewamdrij@gmail.com>
2023-03-20 13:08:50 +0200

    Finish Task number 5

--- Bondarev_IKM-620b_Course_project/Form1.cs
+++ Bondarev_IKM-620b_Course_project/Form1.cs
@@ -68,6 +68,7 @@ namespace Bondarev_IKM_620b_Course_project
         {
             MajorObject = new MajorWork();
             MajorObject.SetTime();
+            MajorObject.Modify = false;// заборона запису
             About A = new About(); // створення форми About
             A.tAbout.Start();
             A.ShowDialog(); // відображення діалогового вікна About 
@@ -99,10 +100,12 @@ namespace Bondarev_IKM_620b_Course_project
 
         private void howSaveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (sfdSave.ShowDialog() == DialogResult.OK)// Виклик діалогового вікна збереження файлу
-{
-                MessageBox.Show(sfdSave.FileName);
+            if (sfdSave.ShowDialog() == DialogResult.OK) // Виклик діалогового вікна збереження
+            {
+                MajorObject.WriteSaveFileName(sfdSave.FileName); // написання імені файлу
+                MajorObject.SaveToFile(); // метод збереження в файл 
             }
+
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
